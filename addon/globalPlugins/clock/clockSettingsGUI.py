6ba00b653689dbe8d27a31d9b81bee69f2dcd3a9
Hrvoje Katić <hrvojekatic@gmail.com>
2018-05-01 10:23:21 +0200

    Redisigned Clock Settings dialog with NVDA's GuiHelper implementation.

--- addon/globalPlugins/clock/clockSettingsGUI.py
+++ addon/globalPlugins/clock/clockSettingsGUI.py
@@ -16,86 +16,37 @@ import os
 import wx
 import addonHandler
 addonHandler.initTranslation()
+
 class clockSettingsDialog(gui.SettingsDialog):
+	# Translators: This is the title of the Clock settings dialog.
 	title=_("Clock Settings")
 
-	def __init__(self, parent):
-		super(clockSettingsDialog, self).__init__(parent)
-
-	def makeSettings(self, sizer):
-
-		#combo box for time display format
-		timeDisplaySizer=wx.BoxSizer(wx.HORIZONTAL)
-		timeDisplayFormatLabel=wx.StaticText(self, label=_("&Time display format:"))
-		timeDisplaySizer.Add(timeDisplayFormatLabel)
-		self._timeDisplayFormatChoice=wx.Choice(self, choices=[datetime.now().strftime(x) for x in formats.timeDisplayFormats])
-		timeDisplaySizer.Add(self._timeDisplayFormatChoice)
-		sizer.Add(timeDisplaySizer)
-
-		#combo box for date display format
-		dateDisplaySizer=wx.BoxSizer(wx.HORIZONTAL)
-		dateDisplayFormatLabel=wx.StaticText(self, label=_("&Date display format:"))
-		dateDisplaySizer.Add(dateDisplayFormatLabel)
-		self._dateDisplayFormatChoice=wx.Choice(self, choices=[datetime.now().strftime(x) for x in formats.dateDisplayFormats])
-		dateDisplaySizer.Add(self._dateDisplayFormatChoice)
-		sizer.Add(dateDisplaySizer)
-
-		#check box for 24-hour input
-		self.input24HourFormatCheckBox=wx.CheckBox(self, label=_("Input in &24-hour format"))
-
-		#combo box for automatic time announcement
-		autoAnnounceSizer=wx.BoxSizer(wx.HORIZONTAL)
-		autoAnnounceLabel=wx.StaticText(self, label=_("Auto announce &interval:"))
-		autoAnnounceSizer.Add(autoAnnounceLabel)
+	def makeSettings(self, settingsSizer):
+		clockSettingsGuiHelper = gui.guiHelper.BoxSizerHelper(self, sizer=settingsSizer)
+		# Translators: This is the label for a combo box in the Clock settings dialog.
+		self._timeDisplayFormatChoice = clockSettingsGuiHelper.addLabeledControl(_("&Time display format:"), wx.Choice, choices=[datetime.now().strftime(x) for x in formats.timeDisplayFormats])
+		# Translators: This is the label for a combo box in the Clock settings dialog.
+		self._dateDisplayFormatChoice = clockSettingsGuiHelper.addLabeledControl(_("&Date display format:"), wx.Choice, choices=[datetime.now().strftime(x) for x in formats.dateDisplayFormats])
+		# Translators: This is the label for a checkbox in the Clock settings dialog.
+		self.input24HourFormatCheckBox = clockSettingsGuiHelper.addItem(wx.CheckBox(self, label=_("Input in &24-hour format")))
 		autoAnnounceChoices=(_("off"), _("every 10 minutes"), _("every 15 minutes"), _("every 30 minutes"), _("every hour"))
-		self._autoAnnounceChoice=wx.Choice(self, choices=autoAnnounceChoices)
+		# Translators: This is the label for a combo box in the Clock settings dialog.
+		self._autoAnnounceChoice = clockSettingsGuiHelper.addLabeledControl(_("Auto announce &interval:"), wx.Choice, choices=autoAnnounceChoices)
 		self._autoAnnounceChoice.Bind(wx.EVT_CHOICE, self.onAutoAnnounce)
-		autoAnnounceSizer.Add(self._autoAnnounceChoice)
-		sizer.Add(autoAnnounceSizer)
-
-		#combo box for time reporting
-		timeReportSizer=wx.BoxSizer(wx.HORIZONTAL)
-		timeReportLabel=wx.StaticText(self, label=_("Time &announcement:"))
-		timeReportSizer.Add(timeReportLabel)
 		timeReportChoices=(_("speech and sound"), _("speech only"), _("sound only"))
-		self._timeReportChoice=wx.Choice(self, choices=timeReportChoices)
-		timeReportSizer.Add(self._timeReportChoice)
-		sizer.Add(timeReportSizer)
-
-		#combo box for chime sound
-		timeReportSoundSizer=wx.BoxSizer(wx.HORIZONTAL)
-		timeReportSoundLabel=wx.StaticText(self, label=_("Clock chime &sound:"))
-		timeReportSoundSizer.Add(timeReportSoundLabel)
-		self._timeReportSoundChoice=wx.Choice(self, choices=paths.LIST_SOUNDS)
+		# Translators: This is the label for a combo box in the Clock settings dialog.
+		self._timeReportChoice = clockSettingsGuiHelper.addLabeledControl(_("Time &announcement:"), wx.Choice, choices=timeReportChoices)
+		# Translators: This is the label for a combo box in the Clock settings dialog.
+		self._timeReportSoundChoice = clockSettingsGuiHelper.addLabeledControl(_("Clock chime &sound:"), wx.Choice, choices=paths.LIST_SOUNDS)
 		self._timeReportSoundChoice.SetStringSelection(config.conf["clockAndCalendar"]["timeReportSound"])
 		self._timeReportSoundChoice.Bind(wx.EVT_CHOICE, self.onSoundSelected)
-		timeReportSoundSizer.Add(self._timeReportSoundChoice)
-		sizer.Add(timeReportSoundSizer)
-
-		#quiet hours sizer
-		self.quietHoursSizer=wx.BoxSizer(wx.VERTICAL)
-
-		#check box for quiet hours
-		self.quietHoursCheckBox=wx.CheckBox(self, label=_("&Quiet hours"))
+		# Translators: This is the label for a checkbox in the Clock settings dialog.
+		self.quietHoursCheckBox = clockSettingsGuiHelper.addItem(wx.CheckBox(self, label=_("&Quiet hours")))
 		self.quietHoursCheckBox.Bind(wx.EVT_CHECKBOX, self.onQuietHoursToggle)
-		self.quietHoursSizer.Add(self.quietHoursCheckBox)
-
-		#edit box for quiet hours start time
-		self.quietStartTimeSizer=wx.BoxSizer(wx.HORIZONTAL)
-		quietStartTimeLabel=wx.StaticText(self, label=_("Quiet hours start time:"))
-		self.quietStartTimeSizer.Add(quietStartTimeLabel)
-		self.quietStartTimeText=wx.TextCtrl(self)
-		self.quietStartTimeSizer.Add(self.quietStartTimeText)
-		self.quietHoursSizer.Add(self.quietStartTimeSizer)
-
-		#edit box for quiet hours end time
-		self.quietEndTimeSizer=wx.BoxSizer(wx.HORIZONTAL)
-		quietEndTimeLabel=wx.StaticText(self, label=_("Quiet hours end time:"))
-		self.quietEndTimeSizer.Add(quietEndTimeLabel)
-		self.quietEndTimeText=wx.TextCtrl(self)
-		self.quietEndTimeSizer.Add(self.quietEndTimeText)
-		self.quietHoursSizer.Add(self.quietEndTimeSizer)
-		sizer.Add(self.quietHoursSizer)
+		# Translators: This is the label for an edit field in the Clock settings dialog.
+		self.quietStartTimeText = clockSettingsGuiHelper.addLabeledControl(_("Quiet hours start time:"), wx.TextCtrl)
+		# Translators: This is the label for an edit field in the Clock settings dialog.
+		self.quietEndTimeText = clockSettingsGuiHelper.addLabeledControl(_("Quiet hours end time:"), wx.TextCtrl)
 
 	def onAutoAnnounce(self, evt):
 		evt.Skip()
@@ -112,17 +63,21 @@ class clockSettingsDialog(gui.SettingsDialog):
 
 	def checkAutoAnnounceSetup(self):
 		if self._autoAnnounceChoice.GetSelection()==0:
-			self.quietHoursSizer.ShowItems(show=False)
+			self.quietHoursCheckBox.Show(False)
+			self.quietStartTimeText.Show(False)
+			self.quietEndTimeText.Show(False)
 		else:
-			self.quietHoursSizer.ShowItems(show=True)
+			self.quietHoursCheckBox.Show(True)
+			self.quietStartTimeText.Show(True)
+			self.quietEndTimeText.Show(True)
 
 	def checkQuietHoursSetup(self):
-		if self.quietHoursCheckBox.GetValue() and self.quietHoursSizer.IsShown(True):
-			self.quietStartTimeSizer.ShowItems(show=True)
-			self.quietEndTimeSizer.ShowItems(show=True)
-		elif not self.quietHoursCheckBox.GetValue() and self.quietHoursSizer.IsShown(True):
-			self.quietStartTimeSizer.ShowItems(show=False)
-			self.quietEndTimeSizer.ShowItems(show=False)
+		if self.quietHoursCheckBox.GetValue() and self._autoAnnounceChoice.GetSelection()!=0:
+			self.quietStartTimeText.Show(True)
+			self.quietEndTimeText.Show(True)
+		elif not self.quietHoursCheckBox.GetValue():
+			self.quietStartTimeText.Show(False)
+			self.quietEndTimeText.Show(False)
 
 	def postInit(self):
 		try:
@@ -144,7 +99,6 @@ class clockSettingsDialog(gui.SettingsDialog):
 		self.checkQuietHoursSetup()
 
 	def onOk(self, evt):
-		super(clockSettingsDialog, self).onOk(evt)
 		config.conf["clockAndCalendar"]["timeDisplayFormat"]=formats.timeDisplayFormats[self._timeDisplayFormatChoice.GetSelection()]
 		config.conf["clockAndCalendar"]["dateDisplayFormat"]=formats.dateDisplayFormats[self._dateDisplayFormatChoice.GetSelection()]
 		config.conf["clockAndCalendar"]["input24HourFormat"]=self.input24HourFormatCheckBox.GetValue()
@@ -154,3 +108,4 @@ class clockSettingsDialog(gui.SettingsDialog):
 		config.conf["clockAndCalendar"]["quietHours"]=self.quietHoursCheckBox.GetValue()
 		config.conf["clockAndCalendar"]["quietHoursStartTime"]=self.quietStartTimeText.GetValue()
 		config.conf["clockAndCalendar"]["quietHoursEndTime"]=self.quietEndTimeText.GetValue()
+		super(clockSettingsDialog, self).onOk(evt)
