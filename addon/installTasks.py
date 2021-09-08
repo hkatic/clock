b2a3906b9708b95f25604c99a6a77a67bdfb9a67
Joseph Lee <joseph.lee22590@gmail.com>
2021-09-08 11:31:49 -0700

    Install tasks (Flake8 (tabs) ET126): do not indent closing parenthesis.

--- addon/installTasks.py
+++ addon/installTasks.py
@@ -18,14 +18,14 @@ def onInstall ():
 			_("The date and time format you were using are not compatible with this version of the Clock add-on, this will be fixed during installation. Click OK to confirm these corrections"),
 			# Translators: the title of a message box dialog.
 			_("Time and date format corrections"), wx.OK | wx.ICON_INFORMATION
-			) == wx.OK:
-				try:
-					config.conf['clockAndCalendar']['timeDisplayFormat'] = 0
-					config.conf['clockAndCalendar']['dateDisplayFormat'] = 1
-				except VdtTypeError:
-					config.conf['clockAndCalendar']['timeDisplayFormat'] = "0"
-					config.conf['clockAndCalendar']['dateDisplayFormat'] = "1"
-				finally:
-					# For those who have not checked the "Save configuration on exit" checkbox.
-					if not config.conf["general"]["saveConfigurationOnExit"]:
-						config.conf.save ()
+		) == wx.OK:
+			try:
+				config.conf['clockAndCalendar']['timeDisplayFormat'] = 0
+				config.conf['clockAndCalendar']['dateDisplayFormat'] = 1
+			except VdtTypeError:
+				config.conf['clockAndCalendar']['timeDisplayFormat'] = "0"
+				config.conf['clockAndCalendar']['dateDisplayFormat'] = "1"
+			finally:
+				# For those who have not checked the "Save configuration on exit" checkbox.
+				if not config.conf["general"]["saveConfigurationOnExit"]:
+					config.conf.save ()
