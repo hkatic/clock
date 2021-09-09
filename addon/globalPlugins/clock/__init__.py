af9ca1cb9fc0932aafff513c8f73a2e9dec5e0a4
Joseph Lee <joseph.lee22590@gmail.com>
2021-09-09 09:59:00 -0700

    Confspec: transfer confspec data to the main global plugin module. Re #33.
    Side-effect.hinders lint: when configuration module loads, NVDA config spec is updated to include CLock add-on confspec. This means with the configuration module removed (which is not used apart from the main global plugin module), validator will throw errors at startup. Therefore to ease linting and to align more closely with other add-ons employing confspec, transfer add-on confspec to the main global plugin module and remove configuration module import.

--- addon/globalPlugins/clock/__init__.py
+++ addon/globalPlugins/clock/__init__.py
@@ -33,11 +33,28 @@ from . import convertdate
 sys.path.remove(sys.path[-1])
 import time
 from winKernel import GetTimeFormatEx, GetDateFormatEx
-from . import configuration
 from configobj.validate import VdtTypeError
 
 import addonHandler
 addonHandler.initTranslation()
+
+
+confspec = {
+	'timeDisplayFormat': 'integer(default=0)',
+	'dateDisplayFormat': 'integer(default=1)',
+	'input24HourFormat': 'boolean(default=False)',
+	'autoAnnounce': 'integer(default=0)',
+	'timeReporting': 'integer(default=0)',
+	'timeReportSound': 'string(default="clock_chime1.wav")',
+	'alarmSound': 'string(default="alarm1.wav")',
+	'alarmTimerChoice': 'integer(default=1)',
+	'quietHours': 'boolean(default=False)',
+	'alarmTime': 'float(default=0.0)',
+	'alarmSavedTime': 'float(default=0.0)',
+	'quietHoursStartTime': 'string(default="")',
+	'quietHoursEndTime': 'string(default="")',
+}
+config.conf.spec["clockAndCalendar"] = confspec
 
 
 def secondsToString(seconds):
