224485f42200af146801a12fc1d8ce43d4ab69c1
Joseph Lee <joseph.lee22590@gmail.com>
2021-11-11 19:17:45 -0800

    NVDA compatibility: last tested version -> 2021.3

--- buildVars.py
+++ buildVars.py
@@ -39,7 +39,7 @@ For other instructions, press Add-on help button in add-ons manager."""),
 	# Minimum NVDA version supported (e.g. "2018.3.0", minor version is optional)
 	"addon_minimumNVDAVersion" : "2019.3",
 	# Last NVDA version supported/tested (e.g. "2018.4.0", ideally more recent than minimum version)
-	"addon_lastTestedNVDAVersion" : "2021.2",
+	"addon_lastTestedNVDAVersion" : "2021.3",
 	# Add-on update channel (default is None, denoting stable releases,
 	# and for development releases, use "dev".)
 	# Do not change unless you know what you are doing!
