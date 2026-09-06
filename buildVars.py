dfae584c136acb2308213872a1f57e6e07046fba
zstanecic <zvonimirek222@yandex.com>
2026-09-06 14:23:14 +0200

    addon bumping

--- buildVars.py
+++ buildVars.py
@@ -27,7 +27,7 @@ NVDA+F12 pressed twice quickly, get current date.
 NVDA+F12 pressed three times quickly, reports the current day, the week number, the current year and the remaining days before the end of the year.
 For other instructions, press  actions button in add-on store, then go to help."""),
 	# version
-	addon_version="20260521.0.2",
+	addon_version="20260906.0.0",
 	# Brief changelog for this version
 	# Translators: what's new content for the add-on version to be shown in the add-on store
 	addon_changelog=_(""" Changes for 20260906.0.0.
